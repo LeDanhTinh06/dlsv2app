686e01b5522adc758c3ed0e5956d5d1416872927
LeDanhTinh06 <ledanhtinh06@gmail.com>
2026-09-11 23:46:37 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ const MULT = {
   acc: 1.266,
   sta: 0.55,
   str: 1.55,
-  con: 2.09,
-  pas: 0.9535,
-  sho: 0.9535,
-  tac: 0.9535,
+  con: 2.1,
+  pas: 0.95,
+  sho: 0.95,
+  tac: 0.95,
 };
 
 const LABELS = {
